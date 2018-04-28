531a129f0bc883e599631c3d23c067fd6db080d7
Miguel <miguel.aurelio.murias28@gmail.com>
2018-04-28 16:16:54 +0200

    rubocop

--- lib/view_delegates/poros/view_delegate.rb
+++ lib/view_delegates/poros/view_delegate.rb
@@ -6,9 +6,13 @@ module ViewDelegates
       attr_accessor :delegate_cache
     end
     class_attribute :view_locals
-    class_attribute :models
     class_attribute :ar_models
     class_attribute :properties
+    # We need self, Ruby gets confused without the self and thinks of a local variable instead
+    # of ivar
+    self.view_locals = self.view_locals || []
+    self.ar_models = self.ar_models || []
+    self.properties = self.properties || []
     # View delegate cache system
     # @return [ViewDelegates::Cache]
     def self.delegate_cache
@@ -39,12 +43,12 @@ module ViewDelegates
       end
       self.ar_models = {}
       self.class.ar_models&.each do |ar_model|
-        self.ar_models[ar_model] = instance_variable_get(:"@#{ar_model}")
+        ar_models[ar_model] = instance_variable_get(:"@#{ar_model}")
       end
       self.class.properties&.each do |property|
         locals[property] = instance_variable_get "@#{property}"
       end
-      locals = locals.merge(self.ar_models).merge(local_params)
+      locals = locals.merge(ar_models).merge(local_params)
       result = ViewDelegateController.render(self.class.view_path + '/' + view.to_s,
                                              locals: locals)
 
@@ -66,115 +70,113 @@ module ViewDelegates
       struct.new(*initialize_hash.values_at(*struct_members))
     end
 
-
-    # Override the new, we may need polymorphism
-    # @param [Hash] args
-    def self.new *args
-      if @polymorph_function
-        command = super(*args)
-        klazz = command.instance_eval(&@polymorph_function)
-        if klazz == self
-          super(*args)
-        else
-          klazz.new(*args)
-        end
-      else
-        super
-      end
-    end
-
-    # Activates cache on the view delegate
-    # option must be true/false
-    # size is an optional parameter, controls the max size of the cache array
-    # @param [Boolean] option
-    # @param [Integer] size
-    def self.cache(option, size: 50)
-      if option
-        render_method = instance_method :render
-        @delegate_cache = ViewDelegates::Cache.new(max_size: size)
-        define_method(:render) do |view, local_params: {}, &block|
-          value_key = "#{hash}#{local_params.hash}#{view.to_s}"
-          result = self.class.delegate_cache.get value_key
-          if result.nil?
-            result = render_method.bind(self).call(view, local_params: local_params)
-            self.class.delegate_cache.add key: value_key, value: result
-          end
-          if block
-            block.call(result)
+    class << self
+      # Override the new, we may need polymorphism
+      # @param [Hash] args
+      def new *args
+        if @polymorph_function
+          command = super(*args)
+          klazz = command.instance_eval(&@polymorph_function)
+          if klazz == self
+            super(*args)
           else
-            result
+            klazz.new(*args)
+          end
+        else
+          super
+        end
+      end
+
+      # Activates cache on the view delegate
+      # option must be true/false
+      # size is an optional parameter, controls the max size of the cache array
+      # @param [Boolean] option
+      # @param [Integer] size
+      def cache(option, size: 50)
+        if option
+          render_method = instance_method :render
+          @delegate_cache = ViewDelegates::Cache.new(max_size: size)
+          define_method(:render) do |view, local_params: {}, &block|
+            value_key = "#{hash}#{local_params.hash}#{view.to_s}"
+            result = self.class.delegate_cache.get value_key
+            if result.nil?
+              result = render_method.bind(self).call(view, local_params: local_params)
+              self.class.delegate_cache.add key: value_key, value: result
+            end
+            if block
+              block.call(result)
+            else
+              result
+            end
           end
         end
       end
-    end
 
-    # Gets the path for the delegate views
-    def self.view_path
-      @view_path ||= to_s.sub(/Delegate/, ''.freeze).underscore
-    end
+      # Gets the path for the delegate views
+      def view_path
+        @view_path ||= to_s.sub(/Delegate/, ''.freeze).underscore
+      end
 
-    # Marks a method as a view local
-    # @param [Symbol] method
-    def self.view_local(*methods)
-      self.view_locals = [] if view_locals.nil?
-      self.view_locals += methods
-    end
+      # Marks a method as a view local
+      # @param [Symbol] method
+      def view_local(*methods)
+        self.view_locals = [] if view_locals.nil?
+        self.view_locals += methods
+      end
 
-    # View properties
-    # @param [Symbol] method
-    def self.property(*methods)
-      self.properties = [] if self.properties.nil?
-      methods.each do |method|
+      # View properties
+      # @param [Symbol] method
+      def property(*methods)
+        methods.each do |method|
+          attr_accessor method
+        end
+        self.properties += methods
+      end
+
+      # Polymorphism method
+      # The block must return the class we must use
+      # @param [Proc] block
+      def polymorph &block
+        @polymorph_function = block
+      end
+
+      # The models this delegate will use
+      # @param [method] method The variable name this model will use
+      # @param [Array] properties The model properties to extract
+      # from the active record model
+      def model(method, properties: [])
         attr_accessor method
+        # Add the method name to the array of delegate models
+        self.ar_models += [method]
+        # Define a setter for the model
+        define_method "#{method}=" do |val|
+          # Create a struct with the model properties
+          model_delegate = if properties.any?
+                             Struct.new(*properties)
+                           else
+                             Struct.new(*val.attributes.keys)
+                           end
+          model_delegate = model_to_struct(val, model_delegate)
+          # set the struct to instance model
+          instance_variable_set(:"@#{method}", model_delegate)
+        end
       end
-      self.properties += methods
-    end
 
-    # Polymorphism method
-    # The block must return the class we must use
-    # @param [Proc] block
-    def self.polymorph &block
-      @polymorph_function = block
-    end
-
-    # The models this delegate will use
-    # @param [method] method The variable name this model will use
-    # @param [Array] properties The model properties to extract
-    # from the active record model
-    def self.model(method, properties: [])
-      attr_accessor method
-      self.ar_models = [] if self.ar_models.nil?
-      # Add the method name to the array of delegate models
-      self.ar_models += [method]
-      # Define a setter for the model
-      define_method "#{method}=" do |val|
-        # Create a struct with the model properties
-        model_delegate = if properties.any?
-                           Struct.new(*properties)
-                         else
-                           Struct.new(*val.attributes.keys)
-                         end
-        model_delegate = model_to_struct(val, model_delegate)
-        # set the struct to instance model
-        instance_variable_set(:"@#{method}", model_delegate)
-      end
-    end
-
-    def self.model_array method, properties: []
-      attr_accessor method
-      # Add the method name to the array of delegate models
-      self.ar_models = [] if self.ar_models.nil?
-      self.ar_models += [method]
-      # Define a setter for the model
-      define_method "#{method}=" do |model_array|
-        # Create a struct with the model properties
-        model_delegate = if properties.any?
-                           Struct.new(*properties)
-                         else
-                           Struct.new(*val.attributes.keys)
-                         end
-        model_array.map! {|e| model_to_struct(e, model_delegate)}
-        instance_variable_set(:"@#{method}", model_array)
+      def model_array method, properties: []
+        attr_accessor method
+        # Add the method name to the array of delegate models
+        self.ar_models += [method]
+        # Define a setter for the model
+        define_method "#{method}=" do |model_array|
+          # Create a struct with the model properties
+          model_delegate = if properties.any?
+                             Struct.new(*properties)
+                           else
+                             Struct.new(*val.attributes.keys)
+                           end
+          model_array.map! {|e| model_to_struct(e, model_delegate)}
+          instance_variable_set(:"@#{method}", model_array)
+        end
       end
     end
   end
